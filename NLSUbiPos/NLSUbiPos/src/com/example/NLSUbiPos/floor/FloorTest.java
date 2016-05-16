601ec3f76482ea69741a694658d6f4641b3a9ed9
Lin Wang <sjtu12345@126.com>
2016-05-16 16:05:46 +0800

    楼层识别测试

--- NLSUbiPos/NLSUbiPos/src/com/example/NLSUbiPos/floor/FloorTest.java
+++ NLSUbiPos/NLSUbiPos/src/com/example/NLSUbiPos/floor/FloorTest.java
@@ -1,7 +1,12 @@
 package com.example.NLSUbiPos.floor;
 
 import java.util.Calendar;
+
+import com.example.NLSUbiPos.position.ParticlePosition;
+import com.example.NLSUbiPos.position.Position;
+import com.example.NLSUbiPos.position.PositionClient;
 import com.example.fusionnavigation.R;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -18,27 +23,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-//this is an activity which tests FloorRecognition.java
-public class FloorActivity extends Activity implements SensorEventListener{
+//this is an activity which tests onFloorListener
+public class FloorTest extends Activity implements OnFloorListener{
 	private SensorManager sensorManager = null;
 	private Button startButton;
 	private Button stopButton;
 	private Button ExitButton;
 	private TextView showText;
 	private EditText floorText;
-	private PressureFloorDetector pfd;
+	private PositionClient pc;
+	private int floornum;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
-		pfd=new PressureFloorDetector();
+		pc=new PositionClient(this);
+		pc.getFloorDetector().addOnFloorListener(this);
 		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_floor);
-		
-		// sensors
-		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		final Sensor pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-		// budgets
+		setContentView(R.layout.activity_floor);
 		showText = (TextView) findViewById(R.id.showText);
 		startButton = (Button) findViewById(R.id.Start);
 		stopButton = (Button) findViewById(R.id.Stop);
@@ -47,36 +49,31 @@ public class FloorActivity extends Activity implements SensorEventListener{
 		startButton.setOnClickListener(new OnClickListener(){
 			@SuppressLint("ShowToast") @Override
 			public void onClick(View v) {
-				sensorManager.registerListener(FloorActivity.this, pressure, SensorManager.SENSOR_DELAY_GAME);
+
+			pc.getFloorDetector().setinifloor(Integer.parseInt(floorText.getText().toString()));
+	        pc.registerSensorListener();
+	 
 				startButton.setEnabled(false);
 				stopButton.setEnabled(true);
 				ExitButton.setEnabled(true);
-				
-				if(floorText.getText().toString().length()==0)
-				{
-				Toast.makeText(FloorActivity.this, "璇峰～鍐欐ゼ灞傛暟", Toast.LENGTH_LONG).show();
-				
-				}
-				
-				else{
-				pfd.setinifloor(Integer.parseInt(floorText.getText().toString()));
-
+//				showText.append("You are at "+floornum+" floor");  
 			
 			}
-			}
-		}
-		);
+		});
+		
 		
 		
 		stopButton.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
-				sensorManager.unregisterListener(FloorActivity.this);
-				pfd.pressureList.clear();
+				pc.unregisterSensorListener();
+				pc.getFloorDetector().removeOnFloorListener();
+			
 				startButton.setEnabled(true);
 				stopButton.setEnabled(false);
 				ExitButton.setEnabled(true);
+			
 			}
 		});
 		
@@ -84,9 +81,8 @@ public class FloorActivity extends Activity implements SensorEventListener{
 
 			@Override
 			public void onClick(View v) {
-				sensorManager.unregisterListener(FloorActivity.this);
 				finish();
-				//onDestroy();
+		
 			}
 		});
 		
@@ -94,45 +90,25 @@ public class FloorActivity extends Activity implements SensorEventListener{
 		
 	}
 	
+	
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
-		sensorManager.unregisterListener(FloorActivity.this);
+//		sensorManager.unregisterListener(FloorActivity.this);
 		startButton.setEnabled(true);
 		stopButton.setEnabled(false);
-	}
-
-	private void updateMessage() {
 		
-//		pfd.pressureSize=0;
-		Calendar now = Calendar.getInstance();
-		String time = "" + now.get(Calendar.YEAR) + ":" + (now.get(Calendar.MONTH)+1) + ":" + now.get(Calendar.DAY_OF_MONTH) + 
-				":" + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND);
-	     
-		int floornum=pfd.getFloor();
-	    showText.append(time+"->Floornum="+floornum+"\n");
-//	    this.onPause();
-	    
-
 	}
 
 	@Override
-	public void onSensorChanged(SensorEvent event) {
-		pfd.onSensorChanged(event);
-//		System.out.println(pfd.pressureSize);
-		if(pfd.getpresize()>=50 ) {
-			updateMessage();
-			pfd.notifyFloorEvent(pfd.getFloor());
-		}
-		
-		
+	public void onFloor(int floor) {
+	
+		this.floornum=floor;
 	}
 
 
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO 鑷姩鐢熸垚鐨勬柟娉曞瓨鏍?
-		
-	}
+
+
+
 }
